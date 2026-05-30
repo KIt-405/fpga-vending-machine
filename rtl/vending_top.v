bf5e80b9cce7ead1c6c8ce3d8c5fab668f7ce5d7
CHEN BOON KIT <k03963369@gmail.com>
2026-05-30 16:02:12 +0800

    Update vending_top.v

--- rtl/vending_top.v
+++ rtl/vending_top.v
@@ -5,34 +5,47 @@ module vending_top (
     input wire coin_10c,
     input wire coin_25c,
     output wire dispense,
-    output wire [7:0] change_out
+    output wire [7:0] change_out,
+    output wire [7:0] coin_count
 );
 
-    wire clear_acc_wire;
-    wire total_reached_wire;
-    wire [7:0] current_total_wire;
+wire clear_acc_wire;
+wire total_reached_wire;
+wire [7:0] current_total_wire;
 
-    datapath U_DATAPATH (
-        .clk(clk),
-        .rst(rst),
-        .coin_5c(coin_5c),
-        .coin_10c(coin_10c),
-        .coin_25c(coin_25c),
-        .clear_acc(clear_acc_wire),
-        .current_total(current_total_wire),
-        .total_reached(total_reached_wire)
-    );
+datapath U_DATAPATH(
+    .clk(clk),
+    .rst(rst),
+    .coin_5c(coin_5c),
+    .coin_10c(coin_10c),
+    .coin_25c(coin_25c),
+    .clear_acc(clear_acc_wire),
+    .current_total(current_total_wire),
+    .total_reached(total_reached_wire)
+);
 
-    fsm_controller U_FSM (
-        .clk(clk),
-        .rst(rst),
-        .total_reached(total_reached_wire),
-        .clear_acc(clear_acc_wire),
-        .dispense(dispense)
-    );
+fsm_controller U_FSM(
+    .clk(clk),
+    .rst(rst),
+    .total_reached(total_reached_wire),
+    .clear_acc(clear_acc_wire),
+    .dispense(dispense)
+);
 
-    assign change_out = (dispense && (current_total_wire >= 8'd45)) 
-                        ? (current_total_wire - 8'd45) 
-                        : 8'd0;
+coin_counter U_COUNTER(
+    .clk(clk),
+    .rst(rst),
+    .coin_inserted(
+        coin_5c ||
+        coin_10c ||
+        coin_25c
+    ),
+    .coin_count(coin_count)
+);
+
+assign change_out =
+    (dispense && (current_total_wire >= 8'd45))
+    ? (current_total_wire - 8'd45)
+    : 8'd0;
 
 endmodule
