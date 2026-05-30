1e9c48079e02a0360f7dc5fe344abeb71de55636
CHEN BOON KIT <k03963369@gmail.com>
2026-05-30 16:01:44 +0800

    Update coin_counter.v

--- rtl/coin_counter.v
+++ rtl/coin_counter.v
@@ -1,1 +1,17 @@
+module coin_counter(
+    input wire clk,
+    input wire rst,
+    input wire coin_inserted,
+    output reg [7:0] coin_count
+);
 
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        coin_count <= 8'd0;
+
+    else if(coin_inserted)
+        coin_count <= coin_count + 1'b1;
+end
+
+endmodule
