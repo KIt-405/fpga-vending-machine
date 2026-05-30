50336bab1cc8baf51ada370686c9d8ca30a559bd
CHEN BOON KIT <k03963369@gmail.com>
2026-05-30 16:01:18 +0800

    Update coin_mux.v

--- rtl/coin_mux.v
+++ rtl/coin_mux.v
@@ -1,1 +1,19 @@
+module coin_mux(
+    input wire coin_5c,
+    input wire coin_10c,
+    input wire coin_25c,
+    output reg [7:0] coin_value
+);
 
+always @(*) begin
+    if (coin_25c)
+        coin_value = 8'd25;
+    else if (coin_10c)
+        coin_value = 8'd10;
+    else if (coin_5c)
+        coin_value = 8'd5;
+    else
+        coin_value = 8'd0;
+end
+
+endmodule
