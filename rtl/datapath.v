21965ff43ebc72bcd2cff8122575310b4fbf8bfd
CHEN BOON KIT <k03963369@gmail.com>
2026-05-30 16:01:56 +0800

    Update datapath.v

--- rtl/datapath.v
+++ rtl/datapath.v
@@ -4,30 +4,35 @@ module datapath (
     input wire coin_5c,
     input wire coin_10c,
     input wire coin_25c,
-    input wire clear_acc,       
+    input wire clear_acc,
     output reg [7:0] current_total,
-    output wire total_reached   
+    output wire total_reached
 );
 
-    wire [7:0] coin_value;
+wire [7:0] coin_value;
 
-    assign coin_value = coin_25c ? 8'd25 :
-                        coin_10c ? 8'd10 :
-                        coin_5c  ? 8'd5  : 8'd0;
+coin_mux U_MUX(
+    .coin_5c(coin_5c),
+    .coin_10c(coin_10c),
+    .coin_25c(coin_25c),
+    .coin_value(coin_value)
+);
 
-    always @(posedge clk , posedge rst)
-	 begin
-        if (rst) 
-            current_total <= 8'd0;
-        
-        else if (clear_acc) 
-            current_total <= 8'd0;
-        
-        else if (coin_5c || coin_10c || coin_25c) 
-            current_total <= current_total + coin_value;
-        
-    end
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        current_total <= 8'd0;
 
-    assign total_reached = (current_total >= 8'd45);
+    else if(clear_acc)
+        current_total <= 8'd0;
+
+    else if(coin_5c || coin_10c || coin_25c)
+        current_total <= current_total + coin_value;
+end
+
+comparator U_COMP(
+    .current_total(current_total),
+    .total_reached(total_reached)
+);
 
 endmodule
